164dc5a450d4236e89b4f028d5d24f8b8368c387
CollinsAkoja <collinsakojanathaniels@gmail.com>
2026-02-12 15:44:58 +0000

    "performanc e metrics"

---
+++ performance.py
@@ -0,0 +1,14 @@
+#studenttools/performance.py
+
+class Performance_Analyzer:
+    def __init__(self,average_score, attendance_percentage):
+        self.average_score = average_score
+        self.attendance_percentage = attendance_percentage
+        
+        
+    def performance_summary(self):
+        if self.average_score >= 70 and self.attendance_percentage >= 75:
+            return "Excellent Performance"
+        elif self.average_score >= 50 and self.attendance_percentage >= 60:
+            return "Satisfactory Performance" 
+    
